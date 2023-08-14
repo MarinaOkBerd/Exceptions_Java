1f0b0f04450db54fcf62690bc34d3a919c577629
Marina Berdnikova <berdni4ka@rambler.ru>
2023-08-15 00:05:17 +0300

    Add seminar 2 tasks

---
+++ src/Seminar2/Task_4.java
@@ -0,0 +1,35 @@
+package Seminar2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        InputStream inputStream;
+        try {
+            String[] strings = {"asdf", "asdf"};
+            String str = strings[1];
+            test();
+            int a = 1 / 0;
+            inputStream = new FileInputStream("sdafgdsaf");
+        } catch (StackOverflowError error) {
+            System.out.println("stack overflow error");
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Я все равно выполнился!");
+        }
+        System.out.println("Я жив!");
+    }
+
+    private static void test() throws IOException {
+        File file = new File("1");
+        file.createNewFile();
+        FileReader reader = new FileReader(file);
+        reader.read();
+        test();
+    }
+}
