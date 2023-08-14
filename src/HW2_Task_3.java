1030be6030f913f9215652bc928696787c9b6d37
Marina Berdnikova <berdni4ka@rambler.ru>
2023-08-15 01:03:02 +0300

    Fix error

--- src/HW2_Task_3.java
+++ src/HW2_Task_3.java
@@ -9,9 +9,9 @@ public class HW2_Task_3 {
             printSum(23, 234);
             int[] abc = { 1, 2 };
             abc[3] = 9;
-        } catch (NullPointerException ex) {
-            System.out.println("Массив выходит за пределы своего размера!");
         } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (NullPointerException ex) {
             System.out.println("Указатель не может указывать на null!");
         } catch (Throwable ex) {
             System.out.println("Что-то пошло не так...");
